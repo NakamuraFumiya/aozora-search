8dacce232da57ccd1a8ab0dd36b06d1ec89703fc
NakamuraFumiya <fumiya3691@gmail.com>
2024-04-11 22:35:10 +0900

    cmdディレクトリ配下にaozora-collectorを追加

---
+++ cmd/aozora-collector/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// 作品情報をまとめる構造体
+type Entry struct {
+	AuthorID string
+	Author   string
+	TitleID  string
+	Title    string
+	InfoURL  string
+	ZipURL   string
+}
+
+func findEntries(siteURL string) ([]Entry, error) {
+	// 処理
+	return nil, nil
+}
+
+func main() {
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+
+	entries, err := findEntries(listURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, entry := range entries {
+		fmt.Println(entry.Title, entry.ZipURL)
+	}
+}
